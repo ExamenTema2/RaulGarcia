072afa9ea8f0ee2511243950249caaea45d2bb5d
Mauricio Matamala Peinado <mauricio.politecnico@gmail.com>
2019-12-03 13:59:37 +0100

    Cambios realizados

--- ControlDeFlota/src/Flota.java
+++ ControlDeFlota/src/Flota.java
@@ -3,6 +3,10 @@ import java.util.ArrayList;
 public class Flota {
 	ArrayList<Vehiculo> vehiculos;
 
+	public Flota() {
+		vehiculos = new ArrayList<>();
+	}
+
 	public void add(Vehiculo vehiculo){
 		vehiculos.add(vehiculo);
 	}
